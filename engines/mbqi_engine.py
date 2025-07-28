4b94f29945464c546f20fdff8b7424a3791c7c88
itamar bender <itamar.bender@mail.huji.ac.il>
2025-07-28 10:38:09 +0300

    Add first Implementation of MBQI

--- engines/mbqi_engine.py
+++ engines/mbqi_engine.py
@@ -1,39 +1,162 @@
 from z3 import *
-from typing import List
+from typing import Dict, List, Set, Tuple, Optional, Any, Union
 from engines.quantifier_instantiation_engine import  QuantifierInstantiationEngine
+import itertools
 
 class MBQIEngine(QuantifierInstantiationEngine):
-    def is_applicable(self) -> bool:
-        return is_quantifier(self.formula) and self.formula.is_forall()
+    """
+    Implements Model-Based Quantifier Instantiation for Z3.
+    """
 
-    def find_instantiations(self) -> List[ExprRef]:
-        quant = self.formula
+    def __init__(self, formula, model, max_iterations: int = 100, verbose: bool = False):
+        super().__init__(formula, model)
+        self.max_iterations = max_iterations
+        self.verbose = verbose
+        self.solver = z3.Solver()
+        self.quantifiers = []
+        self.ground_formulas = []
+        self.instantiated_terms = set()
 
-        # Get variables and body
-        num_vars = quant.num_vars()
-        vars = [Const(quant.var_name(i), quant.var_sort(i)) for i in range(num_vars)]
-        body = quant.body()
-
-        # Reverse vars to match De Bruijn indexing
-        subst_body = substitute_vars(body, *reversed(vars))
-
-        # Evaluate body under model with current variable values
-        instantiation = []
-        substitutions = []
-
-        for v in vars:
-            val = self.model.eval(v, model_completion=True)
-            substitutions.append(val)
-
-        # Plug in model values into the formula
-        ground = substitute_vars(body, *reversed(substitutions))
-
-        s = Solver()
-        s.add(Not(ground))  # Check if body fails under model
-
-        if s.check() == sat:
-            # Counterexample found — return the instantiation
-            instantiated = substitute_vars(body, *reversed(substitutions))
-            return [substitute_vars(quant, *reversed(substitutions))]
+        # Preprocess formula (if it's a list, add all)
+        if isinstance(formula, list):
+            for f in formula:
+                self.add_formula(f)
         else:
+            self.add_formula(formula)
+
+    def add_formula(self, formula):
+        if self._is_quantifier(formula):
+            self.quantifiers.append(formula)
+        else:
+            self.ground_formulas.append(formula)
+            self.solver.add(formula)
+
+    def _is_quantifier(self, formula) -> bool:
+        return z3.is_quantifier(formula)
+
+    def _get_model(self) -> Optional[z3.ModelRef]:
+        if self.solver.check() == z3.sat:
+            return self.solver.model()
+        return None
+
+    def _get_universe_values(self, model: z3.ModelRef, sort: z3.SortRef) -> List:
+        universe = []
+        for decl in model.decls():
+            if decl.range() == sort:
+                universe.append(model.get_interp(decl))
+
+        if sort.is_int():
+            universe.extend([z3.IntVal(i) for i in range(-5, 6)])
+        elif sort.is_real():
+            universe.extend([z3.RealVal(i) for i in range(-5, 6)])
+            universe.extend([z3.RealVal(i)/2 for i in range(-5, 6)])
+        elif sort.is_bool():
+            universe.extend([z3.BoolVal(True), z3.BoolVal(False)])
+
+        return list(set(map(str, universe)))
+
+    def _instantiate_quantifier(self, quantifier, model: z3.ModelRef) -> List[z3.ExprRef]:
+        num_vars = quantifier.num_vars()
+        var_sorts = [quantifier.var_sort(i) for i in range(num_vars)]
+        var_names = [quantifier.var_name(i) for i in range(num_vars)]
+        body = quantifier.body()
+        universe_values = [self._get_universe_values(model, sort) for sort in var_sorts]
+
+        instantiations = []
+        for values in itertools.product(*universe_values):
+            if tuple(values) in self.instantiated_terms:
+                continue
+            self.instantiated_terms.add(tuple(values))
+            subst = []
+
+            for name, sort, value in zip(var_names, var_sorts, values):
+                if sort.is_int():
+                    val = z3.IntVal(int(value))
+                elif sort.is_real():
+                    if '/' in value:
+                        num, denom = value.split('/')
+                        val = z3.RealVal(int(num)) / z3.RealVal(int(denom))
+                    else:
+                        val = z3.RealVal(float(value))
+                elif sort.is_bool():
+                    val = z3.BoolVal(value == 'True')
+                else:
+                    val = None
+                    for decl in model.decls():
+                        if str(model.get_interp(decl)) == value:
+                            val = model.get_interp(decl)
+                            break
+                    if val is None:
+                        continue
+                const = z3.Const(name, sort)
+                subst.append((const, val))
+
+            instance = z3.substitute(body, subst)
+            if self.verbose:
+                print(f"Generated instance: {instance}")
+            instantiations.append(instance)
+
+        return instantiations
+
+    def find_instantiations(self) -> List[Any]:
+        model = self._get_model()
+        if not model:
             return []
+
+        all_instances = []
+        for q in self.quantifiers:
+            instances = self._instantiate_quantifier(q, model)
+            all_instances.extend(instances)
+        return all_instances
+
+    def is_applicable(self) -> bool:
+        return len(self.quantifiers) > 0
+
+    def solve(self) -> Tuple[z3.CheckSatResult, Optional[z3.ModelRef]]:
+        iteration = 0
+        while iteration < self.max_iterations:
+            if self.verbose:
+                print(f"MBQI Iteration {iteration}")
+
+            check_result = self.solver.check()
+            if check_result != z3.sat:
+                return check_result, None
+
+            model = self.solver.model()
+            all_satisfied = True
+            new_instances = []
+
+            for quantifier in self.quantifiers:
+                is_universal = z3.is_quantifier(quantifier) and not quantifier.is_exists()
+                instances = self._instantiate_quantifier(quantifier, model)
+
+                for instance in instances:
+                    value = model.eval(instance, model_completion=True)
+                    if (is_universal and value == z3.BoolVal(False)) or \
+                       (not is_universal and value == z3.BoolVal(True)):
+                        if is_universal:
+                            new_instances.append(instance)
+                            all_satisfied = False
+
+            if all_satisfied:
+                return z3.sat, model
+
+            for instance in new_instances:
+                self.solver.add(instance)
+
+            iteration += 1
+
+        return z3.unknown, None
+
+    def check_sat(self) -> z3.CheckSatResult:
+        result, _ = self.solve()
+        return result
+
+    def get_model(self) -> Optional[z3.ModelRef]:
+        result, model = self.solve()
+        return model if result == z3.sat else None
+
+
+def mbqi_solve(formulas, max_iterations=100, verbose=False) -> Tuple[z3.CheckSatResult, Optional[z3.ModelRef]]:
+    mbqi = MBQIEngine(formulas, {}, max_iterations=max_iterations, verbose=verbose)
+    return mbqi.solve()
